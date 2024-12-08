d9df7bb28a0a63bbe0c22e569e6291e8f4ad8967
aslanmmedov <aslanzm-azmp202@code.edu.az>
2024-12-08 01:24:15 -0800

    commit changes

---
+++ ticket-app/src/helpers/admin-helper.js
@@ -0,0 +1,11 @@
+const BASE_URL = "https://warm-west-marlin.glitch.me";
+
+const endpoints = {
+  events: "/events",
+  students: "/venues",
+  orders: "/orders",
+  users: "/users"
+};
+
+export { endpoints };
+export default BASE_URL;
