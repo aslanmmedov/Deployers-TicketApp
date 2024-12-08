0df2959653b07b17a1d957cf0b6fe972b053a2ca
aslanmmedov <aslanzm-azmp202@code.edu.az>
2024-12-07 23:36:27 -0800

    functions added

---
+++ ticket-app/src/helpers/index.js
@@ -0,0 +1,7 @@
+export function removeNavbarItem(className) {
+    className.classList.add('hidden')
+}
+
+export function addNavbarItem(className) {
+    className.classList.remove('hidden')
+}
---
+++ ticket-app/src/services/api/index.js
@@ -0,0 +1,71 @@
+const baseUrl = "https://warm-west-marlin.glitch.me"; 
+
+export const get = async (endpoint) => {
+    try {
+        const response = await fetch(`${baseUrl}${endpoint}`);
+        if (!response.ok) {
+            throw new Error(`GET request failed: ${response.statusText}`);
+        }
+        return await response.json();
+    } catch (error) {
+        console.error(error);
+        throw error;
+    }
+};
+
+export const post = async (endpoint, data) => {
+    try {
+        const response = await fetch(`${baseUrl}${endpoint}`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data),
+        });
+        if (!response.ok) {
+            throw new Error(`POST request failed: ${response.statusText}`);
+        }
+        return await response.json();
+    } catch (error) {
+        console.error(error);
+        throw error;
+    }
+};
+
+export const patch = async (endpoint, data) => {
+    try {
+        const response = await fetch(`${baseUrl}${endpoint}`, {
+            method: "PATCH",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data),
+        });
+        if (!response.ok) {
+            throw new Error(`PATCH request failed: ${response.statusText}`);
+        }
+        return await response.json();
+    } catch (error) {
+        console.error(error);
+        throw error;
+    }
+};
+
+export const del = async (endpoint) => {
+    try {
+        const response = await fetch(`${baseUrl}${endpoint}`, {
+            method: "DELETE",
+        });
+
+        console.log('Response Status:', response.status);
+        console.log('Response Body:', await response.text());
+
+        if (!response.ok) {
+            throw new Error(`DELETE request failed: ${response.statusText} at ${baseUrl}${endpoint}`);
+        }
+    } catch (error) {
+        console.error('Error in DELETE request:', error);
+        throw error;
+    }
+};
+
